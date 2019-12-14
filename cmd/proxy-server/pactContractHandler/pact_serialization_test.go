2d508dbd368ac1a79bf8887996bc88808def8139
Matthew Constable <bonsty@gmail.com>
2019-12-14 22:22:17 +0000

    Add proper serialization type for Pact interactions.

---
+++ cmd/proxy-server/pactContractHandler/pact_serialization_test.go
@@ -0,0 +1,85 @@
+package pactContractHandler
+
+import (
+	"encoding/json"
+	"github.com/mcon/pact-serialization-proxy/cmd/proxy-server/serialization"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCheckSerializationMatchesPactCore(t *testing.T) {
+	expected_data_structure := &serialization.ProviderServiceInteraction{
+		Request: serialization.ProviderServiceRequest{
+			Method: "POST",
+			Path:   "foo/bar",
+			Query:  "?number=1",
+			Encoding: serialization.SerializationEncoding{
+				Type: "protobuf",
+				Description: &serialization.ProtobufEncodingDescription{
+					MessageName:       "BarRequestMessage",
+					FileDescriptorSet: []float64{1, 2, 3},
+				},
+			},
+			Headers: map[string]string{"Accept": "application/octet-stream"},
+			Body:    "{\"Key\" : \"Value\"}",
+		},
+		Response: serialization.ProviderServiceResponse{
+			Status: 200,
+			Encoding: serialization.SerializationEncoding{
+				Type: "protobuf",
+				Description: &serialization.ProtobufEncodingDescription{
+					MessageName:       "BarResponseMessage",
+					FileDescriptorSet: []float64{4, 5, 6},
+				},
+			},
+			Headers: map[string]string{"Accept": "application/octet-stream"},
+			Body:    "",
+		},
+	}
+	json_under_test :=
+		`{
+    "Request": {
+        "Method": "POST",
+        "Path": "foo/bar",
+        "Query": "?number=1",
+        "Encoding": {
+            "Type": "protobuf",
+            "Description": {
+                "MessageName": "BarRequestMessage",
+                "FileDescriptorSet": [
+                    1,
+                    2,
+                    3
+                ]
+            }
+        },
+        "Headers": {
+            "Accept": "application/octet-stream"
+        },
+        "Body": "{\"Key\" : \"Value\"}"
+    },
+    "Response": {
+        "Status": 200,
+        "Encoding": {
+            "Type": "protobuf",
+            "Description": {
+                "MessageName": "BarResponseMessage",
+                "FileDescriptorSet": [
+                    4,
+                    5,
+                    6
+                ]
+            }
+        },
+        "Headers": {
+            "Accept": "application/octet-stream"
+        },
+        "Body": ""
+    }
+}`
+	var unmarshalled_interaction = new(serialization.ProviderServiceInteraction)
+	e := json.Unmarshal([]byte(json_under_test), unmarshalled_interaction)
+	assert.Nil(t, e, "Unmarshaling JSON should succeed")
+
+	assert.Equal(t, expected_data_structure, unmarshalled_interaction, "Expected DTO to serialize into correct form")
+}
---
+++ cmd/proxy-server/serialization/pact_serialization.go
@@ -0,0 +1,34 @@
+package serialization
+
+type ProtobufEncodingDescription struct {
+	MessageName       string
+	FileDescriptorSet []float64
+}
+
+// In general, the contents of `Description` might be different based on the `Type` of the encoding:
+// for now only protobuf is supported, so don't worry about that.
+type SerializationEncoding struct {
+	Type        string
+	Description *ProtobufEncodingDescription
+}
+
+type ProviderServiceRequest struct {
+	Method   string
+	Path     string
+	Query    string
+	Encoding SerializationEncoding
+	Headers  map[string]string
+	Body     string
+}
+
+type ProviderServiceResponse struct {
+	Status   int32
+	Encoding SerializationEncoding
+	Headers  map[string]string
+	Body     string
+}
+
+type ProviderServiceInteraction struct {
+	Request  ProviderServiceRequest
+	Response ProviderServiceResponse
+}
